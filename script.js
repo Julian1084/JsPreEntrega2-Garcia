6dfd545ed3d3ed794da26e613b3158b34bdd499d
Julian García <julian.garcia1084@gmail.com>
2022-12-11 08:10:35 -0300

    script

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const iva = x => x * 1.21
-let opcioncompra = 1
-let cantidad1 = 0
-let precio1 = 861
-let cantidad2 = 0
-let precio2 = 1625
-let cantidad3 = 0
-let precio3 = 1260
-let cantidad4 = 0
-let precio4 = 3000
-let cantidad5 = 0
-let precio5 = 6000
-let cantidad6 = 0
-let precio6= 10000
-let cantidadCuotas = 1
+const descuento = x => x * 0.90
+const recargo = x => x * 1.15
+let productos = [
+{idProducto: 1, nombre: "Picada para 3", presentacion: " ", precio: 3000, categoria: "Picadas"},
+{idProducto: 2, nombre: "Picada para 6", presentacion: " ", precio: 3000, categoria: "Picadas"},
+{idProducto: 3, nombre: "Picada para 10", presentacion: " ", precio: 3000, categoria: "Picadas"},
+{idProducto: 4, nombre: "Picada para 15", presentacion: " ", precio: 3000, categoria: "Picadas"},
+{idProducto: 5, nombre: "Pack de 6: Cerveza Lata Quilmes", presentacion: "Pack de 6 Lata", precio: 861, categoria: "Cerveza"},
+{idProducto: 6, nombre: "Pack de 6: Cerveza Lata Antares", presentacion: "Pack de 6 Lata", precio: 861, categoria: "Cerveza"},
+{idProducto: 7, nombre: "Pack de 6: Cerveza Lata Patagonia",  presentacion: "Pack de 6 Lata", precio: 861, categoria: "Cerveza"},
+{idProducto: 8, nombre: "Pack de 6: Cerveza Lata Andes", presentacion: "Pack de 6 Lata", precio: 861, categoria: "Cerveza"},
+{idProducto: 9, nombre: "Pack de 6: Lata Coca-Cola", precio: 861, presentacion: "Pack de 6 Lata", categoria: "Bebidas sin alcohol"},
+{idProducto: 10, nombre: "Pack de 6: Botella 500cc Aquarius Naranja", presentacion: "Pack de 6 Botella", precio: 861, categoria: "Bebidas sin alcohol"},
+{idProducto: 11, nombre: "Pack Lata Coca-Cola", precio: 861, presentacion: "Pack de 6 Botella", categoria: "Bebidas sin alcohol"}
+]
+let opcionCompra = ""
+let productosFiltrados = []
+let carrito = []
 
 alert("Bienvenido a Camino al Mundial: Picadas")
 let nombre = prompt("Ingrese su nombre").trim().toUpperCase()
@@ -20,31 +23,33 @@ let edad = Number(prompt("Ingrese su edad"))
 
 if (edad >= 18) {
   alert("Bienvenido " + nombre)
-  alert(
-    "A continuación Usted podrá realizar la compra. \nRecuerde que comprando tres o más unidades se realizará un descuento de 10%. \nLos precios no tienen IVA"
-  )
+  alert("A continuación Usted podrá realizar la compra. \nRecuerde que comprando tres o más unidades se realizará un descuento de 10%.")
   do {
-    opcionCompra = prompt(
-      "1- Pack de 6 Cerveza Quilmes: $861 \n2- Pack de 6 Cerveza Patagonia Amber Lager: $1625\n3- Pack de 6 Coca Cola Lata: $1260\n4- Picada para 3: $3000 \n5- Picada para 8 $6000 \n6- Picada para 15 $10.000\n0- Finalizar Compra"
-    );
+    opcionCompra = prompt("Ingrese la categoria a comprar: \n1-Picadas\n2-Cerveza\n3-Bebidas sin alcohol\n0-Para finalizar compra")
     switch (opcionCompra) {
       case "1":
-        cantidad1 = Number(prompt("Ingrese la cantidad de Pack de Cerveza Quilmes a comprar"))
+        productosFiltrados = productos.filter(producto => producto.categoria.includes("Picadas"))
+        productosFiltrados.forEach(producto =>
+          producto.venta = Number(prompt("Ingrese la cantidad de " + producto.nombre + " " + producto.presentacion + "\nPrecio: $ " + producto.precio))
+          )
+          carrito.push(productosFiltrados.filter(producto => producto.venta > 0))
+      
         break
       case "2":
-        cantidad2 = Number(prompt("Ingrese la cantidad de Pack de Cerveza Patagonia a comprar"))
+        productosFiltrados = productos.filter(producto => producto.categoria.includes("Cerveza"))
+        productosFiltrados.forEach(producto =>
+          producto.venta = Number(prompt("Ingrese la cantidad de " + producto.nombre + " " + producto.presentacion + "\nPrecio: $ " + producto.precio))
+          )
+          carrito.push(productosFiltrados.filter(producto => producto.venta > 0))
         break
       case "3":
-        cantidad3 = Number(prompt("Ingrese la cantidad de Pack de Latas de Coca-Cola a comprar"))
-        break
-      case "4":
-        cantidad4 = Number(prompt("Ingrese la cantidad de Picadas para 3 a comprar"))
-        break
-      case "5":
-        cantidad5 = Number(prompt("Ingrese la cantidad de Picadas para 6 a comprar"))
-        break
-      case "6":
-        cantidad6 = Number(prompt("Ingrese la cantidad de Picadas para 15 a comprar"))
+        productosFiltrados = productos.filter(producto => producto.categoria.includes("Bebidas sin alcohol"))
+        productosFiltrados.forEach(producto =>
+          producto.venta = Number(prompt("Ingrese la cantidad de " + producto.nombre + " " + producto.presentacion + "\nPrecio: $ " + producto.precio))
+          )
+          
+          carrito.push(productosFiltrados.filter(producto => producto.venta > 0))
+        
         break
       case "0":
         break
@@ -53,12 +58,15 @@ if (edad >= 18) {
         break
     }
   } while (opcionCompra != 0)
-  if (cantidad1 + cantidad2 + cantidad3 + cantidad4 + cantidad5 + cantidad6 > 0) {
-    let subtotal = cantidad1 * precio1 + cantidad2 * precio2 +cantidad3 * precio3 + cantidad4 * precio4 + cantidad5 * precio5 + cantidad6 * precio6
-    let descuento = 0
+  console.log(carrito)
+  let cantidadTotalComprada = carrito.reduce((acumulador, producto) => acumulador + producto.venta, 0)
+  console.log(cantidadTotalComprada)
+  if (cantidadTotalComprada < 3) {
+    let subtotal =
+    
   
     alert("El subtotal a pagar es $ " + subtotal)
-    if ((cantidad1 + cantidad2 + cantidad3 + cantidad4 + cantidad5 + cantidad6) >= 3) {
+    if (carrito.venta.reduce(acumulador + cantidad, 0) >= 3) {
         descuento = (subtotal * 0.10).toFixed(2)
         alert("Se ha aplicado un descuento de 10% \nSu descuento ha sido de: $ " + descuento)
         
